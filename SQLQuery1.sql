bbdab71d742e17c633519974f24101be32d29a76
VusalAbdurahamnov12 <101477167+VusalAbdurahamnov12@users.noreply.github.com>
2022-04-08 02:20:31 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+--CREATE database test
+--USE test
+--CREATE TABLE Persons (
+--    PersonID int,
+--    LastName varchar(255),
+--    FirstName varchar(255),
+--    Address varchar(255),
+--    City varchar(255)
+--);
+
